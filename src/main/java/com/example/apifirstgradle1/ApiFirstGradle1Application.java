d37b994f131edf6c50a549fe6d0a72b72c2d8691
avrabie <mr.vrabie@gmail.com>
2023-10-06 00:11:50 +0200

    example of api-first with gradle

---
+++ src/main/java/com/example/apifirstgradle1/ApiFirstGradle1Application.java
@@ -0,0 +1,14 @@
+package com.example.apifirstgradle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstGradle1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFirstGradle1Application.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/example/apifirstgradle1/controllers/IakaMyController.java
@@ -0,0 +1,58 @@
+package com.example.apifirstgradle1.controllers;
+
+import org.openapi.example.api.BooksApi;
+import org.openapi.example.model.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class IakaMyController implements BooksApi {
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return BooksApi.super.getRequest();
+    }
+
+    @Override
+    public ResponseEntity<Void> createbook(Book book) {
+        return BooksApi.super.createbook(book);
+    }
+
+    @Override
+    public ResponseEntity<Void> deletebook(String bookId) {
+        return BooksApi.super.deletebook(bookId);
+    }
+
+    @Override
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<Book> getbook(String bookId) {
+        Book book = new Book();
+        book.setIsbn("1234567890");
+        book.setTitle("Iaka");
+        book.setAuthor("Iaka");
+        book.setCost(100.0);
+        return ResponseEntity.ok(book);
+
+    }
+
+    @Override
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<List<Book>> getbooks() {
+        Book book = new Book();
+        book.setIsbn("1234567890");
+        book.setTitle("Iaka");
+        book.setAuthor("Iaka");
+        book.setCost(100.0);
+        return ResponseEntity.ok(List.of(book));
+    }
+
+    @Override
+    public ResponseEntity<Void> updatebook(String bookId, Book book) {
+        return BooksApi.super.updatebook(bookId, book);
+    }
+}
